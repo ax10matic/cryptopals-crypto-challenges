3cafd92aa8be00dbca42f53415eaf56b989409c6
name <me@example.com>
2020-12-08 00:38:17 +0000

    add comments

--- src/libs/base64.c
+++ src/libs/base64.c
@@ -1,9 +1,11 @@
 #include "base64.h"
 
+/* encode byte string to base64 string */
 BASE64 *base64_encode(BYTESTRING *bytes_in){
-    printf("%d\n", strlen(bytes_in));
+    
 }
 
+/* allocate memory for base64 struct */
 BASE64 *new_base64(size_t size){
     BASE64 *b64 = (BASE64*)malloc(sizeof(BASE64));
     if(b64 == NULL){
@@ -12,6 +14,7 @@ BASE64 *new_base64(size_t size){
     }
 
     b64 -> size = size; 
+    // allocate memory for b64 array and initialize it to 0s
     b64 -> b64_str = (char*)malloc(size);
     if(b64 -> b64_str == NULL){
         fprintf(stderr, "Error: cannot malloc b64_str");
--- src/libs/types.c
+++ src/libs/types.c
@@ -1,5 +1,6 @@
 #include "types.h"
 
+/* allocate memory for bytestring struct */
 BYTESTRING *new_bytestring(size_t size){
     BYTESTRING *bstr = (BYTESTRING *)malloc(sizeof(BYTESTRING));
     if(bstr == NULL){
@@ -8,6 +9,7 @@ BYTESTRING *new_bytestring(size_t size){
     }
 
     bstr -> size = size;
+    // allocate memory for uint8_t array  and initialize it to 0s 
     bstr -> bytes = (uint8_t*)malloc(size);
     if(bstr -> bytes == NULL){
         fprintf(stderr, "Error: cannot malloc uint8_t*");
